7c28beb7d660abe1cee80552590cb2a19ec1f62c
Aldcejam <aldcejamj923@gmail.com>
2022-02-17 18:18:52 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'primary': '#E5E5E5',
+      'constrast': '#613EEA',
+      'orange': '#FF7D00',
+      'black':'#171D33'
+    },
+    fontFamily: {
+      sans: ['Sarabun', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
