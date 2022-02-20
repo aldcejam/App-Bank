f5e960fd6af9da2f977a0bd916353fa5a5eb9dd3
Aldcejam <aldcejamj923@gmail.com>
2022-02-20 17:04:10 -0300

    finished footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,44 +3,44 @@ import { useEffect, useState } from "react"
 export function Footer() {
 
 
-    const [home, setHome] = useState(false)
-    const [wallet, setWallet] = useState(false)
-    const [money, setMoney] = useState(false)
-    const [chart, setChart] = useState(false)
-    const [cog, setCog] = useState(false)
+    const [iconHome, setIconHome] = useState(false)
+    const [iconWallet, setIconWallet] = useState(false)
+    const [iconMoney, setIconMoney] = useState(true)
+    const [iconChart, setIconChart] = useState(false)
+    const [iconConfig, setIconConfig] = useState(false)
 
     const activeEvery = (type:string)=>{
-            type == "home"? (home ? setHome(false) : setHome(true),setWallet(false),setMoney(false),setChart(false),setCog(false)) : "";
-            type == "wallet"? (wallet ? setWallet(false) : setWallet(true), setHome(false),setMoney(false),setChart(false),setCog(false)) : "";
-            type == "money"? (money ? setMoney(false) : setMoney(true), setHome(false),setWallet(false),setChart(false),setCog(false)) : "";
-            type == "chart"? (chart ? setChart(false) : setChart(true), setHome(false),setWallet(false),setMoney(false),setCog(false)) : "";
-            type == "cog"? (cog ? setCog(false) : setCog(true), setHome(false),setWallet(false),setMoney(false),setChart(false)) : "";   
+            type == "iconHome"? (iconHome ? setIconHome(false) : setIconHome(true),setIconWallet(false),setIconMoney(false),setIconChart(false),setIconConfig(false)) : "";
+            type == "iconWallet"? (iconWallet ? setIconWallet(false) : setIconWallet(true), setIconHome(false),setIconMoney(false),setIconChart(false),setIconConfig(false)) : "";
+            type == "iconMoney"? (iconMoney ? setIconMoney(false) : setIconMoney(true), setIconHome(false),setIconWallet(false),setIconChart(false),setIconConfig(false)) : "";
+            type == "iconChart"? (iconChart ? setIconChart(false) : setIconChart(true), setIconHome(false),setIconWallet(false),setIconMoney(false),setIconConfig(false)) : "";
+            type == "iconConfig"? (iconConfig ? setIconConfig(false) : setIconConfig(true), setIconHome(false),setIconWallet(false),setIconMoney(false),setIconChart(false)) : "";   
     }
 
     return (
         <div className="bg-secondary w-full max-w-md h-20 flex items-center bottom-0 fixed pt-6">
             <ul className="navegationUl">
-                <li onClick={()=> activeEvery("home")} className={home ? "active" : ""}>
+                <li onClick={()=> activeEvery("iconHome")} className={iconHome ? "active home" : ""}>
                     <a href="#">
                         <span className="navegationIcon"><i className="uil uil-estate"></i></span>
                     </a>
                 </li>
-                <li onClick={()=> activeEvery("wallet")} className={wallet ? "active " : ""}>
+                <li onClick={()=> activeEvery("iconWallet")} className={iconWallet ? "active wallet" : ""}>
                     <a href="#">
                         <span className="navegationIcon"><i className="uil uil-wallet"></i></span>
                     </a>
                 </li>
-                <li onClick={()=> activeEvery("money")} className={money ? "active " : ""}>
+                <li onClick={()=> activeEvery("iconMoney")} className={iconMoney ? "active money" : ""}>
                     <a href="#">
                         <span className="navegationIcon"><i className="uil uil-money-insert"></i></span>
                     </a>
                 </li>
-                <li onClick={()=> activeEvery("chart")} className={chart ? "active " : ""}>
+                <li onClick={()=> activeEvery("iconChart")} className={iconChart ? "active chart" : ""}>
                     <a href="#">
                         <span className="navegationIcon"><i className="uil uil-chart-pie"></i></span>
                     </a>
                 </li>
-                <li onClick={()=> activeEvery("cog")} className={cog ? "active " : ""}>
+                <li onClick={()=> activeEvery("iconConfig")} className={iconConfig ? "active config" : ""}>
                     <a href="#">
                         <span className="navegationIcon"><i className="uil uil-cog"></i></span>
                     </a>
